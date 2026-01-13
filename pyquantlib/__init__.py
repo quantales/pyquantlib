4c7662ecb1e6634069993e1bf7edddbd1165acfa
Yassine Idyiahia <yassine.id@gmail.com>
2026-01-13 01:24:44 +0000

    Fix ruff errors in __init__.py

--- pyquantlib/__init__.py
+++ pyquantlib/__init__.py
@@ -1,27 +1,28 @@
 # pyquantlib/__init__.py
-import sys
 
-from .version import __version__
+from .version import __version__ as __version__
 
 try:
-    from ._pyquantlib import *
     from . import _pyquantlib as _ql
+    from ._pyquantlib import *  # noqa: F403
 except ImportError as e:
-    raise ImportError(f"Failed to import PyQuantLib: {e}")
+    raise ImportError(f"Failed to import PyQuantLib: {e}") from e
 
 # base = _ql.base
 # sys.modules['pyquantlib.base'] = _ql.base
 
 __ql_version__ = _ql.__ql_version__                     # QuantLib C++ version
 __ql_hexversion__ = _ql.__ql_hexversion__
-__boost_version__ = _ql.__boost_version__               # Boost version used at QuantLib compile-time  
+__boost_version__ = _ql.__boost_version__               # Boost version used at QuantLib compile-time
 
 del _ql
+
 
 # Helpers for readable Boost version
 def boost_version_tuple() -> tuple[int, int, int]:
     v = __boost_version__
     return v // 100000, (v // 100) % 1000, v % 100
+
 
 def boost_version_str() -> str:
     return ".".join(map(str, boost_version_tuple()))
