c7f205cad54fddbd64b9cdd3795a605ed840c1eb
Yassine Idyiahia <yassine.id@gmail.com>
2026-01-16 00:36:30 +0000

    Update license headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 /*
  * PyQuantLib: Python bindings for QuantLib
+ * https://github.com/quantales/pyquantlib
+ *
  * Copyright (c) 2025 Yassine Idyiahia
+ * SPDX-License-Identifier: BSD-3-Clause
+ * See LICENSE for details.
  *
+ * ---
  * QuantLib is Copyright (c) 2000-2025 The QuantLib Authors
- * QuantLib is free software under a modified BSD license.
- * See http://quantlib.org/ for more information.
- *
- * Source: https://github.com/quantales/pyquantlib
- * Licensed under the BSD 3-Clause License. See LICENSE file for details.
+ * https://www.quantlib.org/
  */
 
 #include <ql/version.hpp>
